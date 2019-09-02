f03e6d4530e3abea86ad5ec1effaf64c3860f2a1
Morpa <andremorpa@gmail.com>
2019-09-02 15:51:38 +0100

    Highlight Session Finished

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./resources/styles.css";
 
 import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
+import VenueNfo from "./components/venueNfo";
+import Highlight from "./components/highlights";
 
-function App() {
-  return (
-    <div
-      className="App"
-      style={{ height: "1500px", background: "cornflowerblue" }}
-    >
-      <Header />
-      <Featured />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ height: "1500px" }}>
+        <Header />
+        <Featured />
+        <VenueNfo />
+        <Highlight />
+      </div>
+    );
+  }
 }
 
 export default App;
